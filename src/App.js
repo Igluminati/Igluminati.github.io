f304840755c2eaf72f420ec4de436bf21d823398
Igli Kristo <31823943+Igluminati@users.noreply.github.com>
2023-06-26 17:53:58 +0000

    Added App component with a dark-themed layout, including an AppBar, Box components with text content, and animations using the Slide component from react-awesome-reveal.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,105 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Container from "@mui/material/Container";
+import Text from "./components/Text";
+import HomeIcon from "@mui/icons-material/Home";
+import { Slide } from "react-awesome-reveal";
 
-function App() {
+const DarkThemeTeal = createTheme({
+  palette: {
+    primary: {
+      main: "#393E46",
+    },
+    secondary: {
+      main: "#00ADB5",
+    },
+    background: {
+      main: "#222831",
+    },
+    text: {
+      main: "#EEEEEE",
+    },
+  },
+});
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <ThemeProvider theme={DarkThemeTeal}>
+      <AppBar position="static">
+        <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
+          <Button href="/" color="text" variant="text">
+            <HomeIcon />
+          </Button>
+        </Box>
+      </AppBar>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          height: "100vh",
+          bgcolor: "background.main",
+        }}
+      >
+        <Text
+          text="Igli Kristo"
+          color="#00ADB5"
+          fontSize="128px"
+          fontFamily
+          fontStyle
+          fontWeight
+          xPos
+          yPos
+        />
+        <Text
+          text="A highly dedicated and hard working Computer Science student with a strong
+           interest in Software Engineering, Machine Learning and Robotics, seeking an internship
+          opportunity to apply and expand my knowledge and skills in these fields. I aim to make a 
+          positive impact by developing intelligent and efficient solutions through my experience in
+          Software Development, Cloud Platforms and continuous learning in a versatile manner, allowing
+          me to work with many individuals in related fields."
+          color="#00ADB5"
+          fontSize="14px"
+          fontFamily
+          fontStyle
+          fontWeight
+          xPos
+          yPos="80vh"
+        />
+        <Text
+          text="Scroll Down To View My Projects!"
+          color="#762ac1"
+          fontSize="14px"
+          fontFamily
+          fontStyle
+          fontWeight
+          xPos
+          yPos="90vh"
+        />
+      </Box>
+      <Slide>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            height: "50vh",
+            bgcolor: "text.main",
+          }}
         >
-          Learn React!!!
-        </a>
-      </header>
-    </div>
+          <Text
+            text="Igli Kristo"
+            color="#00ADB5"
+            fontSize="128px"
+            fontFamily
+            fontStyle
+            fontWeight
+            xPos
+            yPos
+          />
+        </Box>
+      </Slide>
+    </ThemeProvider>
   );
 }
-
-export default App;
